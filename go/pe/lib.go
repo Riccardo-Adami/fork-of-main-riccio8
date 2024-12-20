a3446412e23687a73f3aba24b9981c72e0ea5d80
Riccardo-Adami <158182544+Riccardo-Adami@users.noreply.github.com>
2024-12-20 09:34:02 +0100

    Update lib.go

--- go/pe/lib.go
+++ go/pe/lib.go
@@ -6,6 +6,12 @@ import(
 )
 
 func load(fileName string) (*pe.File, error){
+	 /*
+	function to load the program as a binary pe, this will return
+	an handle to a file if it succeds and error = nil
+	otherwise it will return an empty handle as first arg and the specific error as second
+	 */
+
 	file, err := pe.Open(fileName)
 	if err != nil {
 		return nil, err
@@ -18,6 +24,12 @@ func load(fileName string) (*pe.File, error){
 
 
 func lib(f *pe.File) ([]string, error){
+	 /*
+	function for finding and reading the libraries of the pe, this will return
+	an array of string if it succeds and error = nil
+	otherwise it will return an empty array as first arg and the specific error as second
+	 */
+	
 	libs, err := f.ImportedLibraries()
 
 	if err != nil {
@@ -28,6 +40,32 @@ func lib(f *pe.File) ([]string, error){
 	
 }
 
+
+func sym(f *pe.File) ([]string, error){
+	 /*
+	function for finding anbd reading the symbols table of the pe, this will return
+	an array of string if it succeds and error = nil
+	otherwise it will return an empty array as first arg and the specific error as second
+	 */
+
+	symblos, err := f.ImportedSymblos()
+
+	if err != nil{
+		return nil, err
+	}
+
+	return symblos, nil
+}
+
+func Sections(f *pe.Fie, name string) (*pe.Section, error){
+	 /*
+	function for finding anbd reading the sections of the pe giving a name, like .text.
+	Section returns the first section with the given name, or nil if no such section exists.
+	 */
+
+	secs, err := 
+	
+}
 
 func main(){
     fmt.Println("working")
